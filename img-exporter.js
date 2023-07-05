e69cabcda2cab5832fbd7c95721fb5c87c50e948
Dani Sanchez <dsanchez@moonpay.com>
2023-07-05 12:02:10 +0200

    cositas

---
+++ img-exporter.js
@@ -0,0 +1,39 @@
+const puppeteer = require("puppeteer");
+const data = require("./src/_data/global.json");
+
+async function captureMultipleScreenshots(urls) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1920, height: 1080 });
+
+  for (let i = 0; i < urls.length; i++) {
+    try {
+      const url = urls[i];
+      await page.goto(url.url);
+      await page.waitForTimeout(2000);
+      await page.screenshot({
+        path: `./src/assets/static/capturas/${url.name}`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  await browser.close();
+}
+
+// const urls = data.map((item) => {
+//   return {
+//     url: item.imageSistema,
+//     name: item.nombreSistema.toLowerCase().replace(/ /g, "_"),
+//   };
+// });
+
+const urls = data.sistemas.map((item) => {
+  return {
+    url: item.imageUrl,
+    name: item.imageSistema,
+  };
+});
+
+captureMultipleScreenshots(urls);
