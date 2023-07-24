f10a09aec342d853ee1bf99b84886a7feb070735
manuelruizredondo <manuelruizredondo@gmail.com>
2023-07-24 09:49:29 +0200

    cambios design

--- img-exporter.js
+++ img-exporter.js
@@ -1,33 +1,34 @@
-const puppeteer = require("puppeteer");
-const data = require("./src/_data/global.json");
+const puppeteer = require('puppeteer');
+const data = require('./src/_data/global.json');
+
+const userAgent =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
 
 async function captureMultipleScreenshots(urls) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless: "new" });
   const page = await browser.newPage();
 
-  await page.setViewport({ width: 1600, height: 1009 });
+  await page.setUserAgent(userAgent);
+  await page.setViewport({ width: 1200, height: 810 });
 
   for (let i = 0; i < urls.length; i++) {
     try {
       const url = urls[i];
+      console.log(`Capturando screenshot ${i + 1} de ${urls.length}`);
       await page.goto(url.url);
       await page.waitForTimeout(2000);
       await page.screenshot({
         path: `./src/assets/static/capturas/${url.name}`,
       });
+      console.log(`Screenshot ${i + 1} capturada: ${url.name}`);
     } catch (error) {
-      console.log(error);
+      console.log(`Error al capturar screenshot ${i + 1}: ${error}`);
     }
   }
-  await browser.close();
-}
 
-// const urls = data.map((item) => {
-//   return {
-//     url: item.imageSistema,
-//     name: item.nombreSistema.toLowerCase().replace(/ /g, "_"),
-//   };
-// });
+  await browser.close();
+  console.log('Proceso de captura de screenshots completado.');
+}
 
 const urls = data.sistemas.map((item) => {
   return {
