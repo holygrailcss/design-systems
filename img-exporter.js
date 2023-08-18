432cf859b473f7c87acf45722258cd67e3cc4850
manuelruizredondo <manuelruizredondo@gmail.com>
2023-08-18 13:24:56 +0200

    cambios y mejoras

--- img-exporter.js
+++ img-exporter.js
@@ -5,24 +5,40 @@ const userAgent =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
 
 async function captureMultipleScreenshots(urls) {
-  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless: "new" });
+  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless: true });
   const page = await browser.newPage();
 
   await page.setUserAgent(userAgent);
   await page.setViewport({ width: 1200, height: 810 });
 
-  for (let i = 0; i < urls.length; i++) {
+  for (let i = 0; i < 2; i++) {
+    const url = urls[i];
     try {
-      const url = urls[i];
-      console.log(`Capturando screenshot ${i + 1} de ${urls.length}`);
-      await page.goto(url.url);
-      await page.waitForTimeout(2000);
-      await page.screenshot({
-        path: `./src/assets/static/capturas/${url.name}`,
-      });
-      console.log(`Screenshot ${i + 1} capturada: ${url.name}`);
+      console.log(`  `);
+      console.log(`- ${i + 1} de ${urls.length}`);
+
+      await page.goto(url.url, { waitUntil: ['load', 'domcontentloaded', 'networkidle0'] });
+
+      const descriptionMetaElement = await page.$('meta[name="description"]');
+      const descriptionMeta = descriptionMetaElement ? await descriptionMetaElement.getProperty('content').then(prop => prop.jsonValue()) : '';
+
+      if (url.cookies) {
+        await page.waitForSelector(`${url.cookies}`, { visible: true });
+        await page.click(`${url.cookies}`);
+        await page.waitForTimeout(6000);
+
+        await page.screenshot({ path: `./src/assets/static/capturas/${url.name}` });
+        console.log(`${url.name}`);
+        console.log(`"descripcionSistema": "${descriptionMeta}",`);
+      } else {
+        await page.waitForTimeout(2000);
+        await page.screenshot({ path: `./src/assets/static/capturas/${url.name}` });
+        console.log(`${url.name}`);
+        console.log(`"descripcionSistema": "${descriptionMeta}",`);
+      }
+
     } catch (error) {
-      console.log(`Error al capturar screenshot ${i + 1}: ${error}`);
+      console.log(`Error al capturar screenshot ${url.name}  ${i + 1}: ${error}`);
     }
   }
 
@@ -34,6 +50,7 @@ const urls = data.sistemas.map((item) => {
   return {
     url: item.imageUrl,
     name: item.imageSistema,
+    cookies: item.cookies
   };
 });
 
